d0945e07c1e4435889073760770f20902d25caff
michalinab <michalina.bartosik@gmail.com>
2016-08-26 11:33:12 +0200

    Poprawki dla secend commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Elo Elo World!");
+        System.out.println("Elo Elo Świat, dobrze Cię widzieć!!!!!");
     }
 }
